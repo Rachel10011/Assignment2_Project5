067afc0e1983d7b004b79ec40d6a0b2d7ec324e4
Rachel Le <71084940+Rachel10011@users.noreply.github.com>
2022-09-21 17:33:42 -0400

    Updated source code to read StudetnData_Emails.txt file

--- Assignment2_Project5/Source.cpp
+++ Assignment2_Project5/Source.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 
+#define PRE_RELEASE
 
 using namespace std;
 
@@ -11,17 +12,21 @@ struct STUDENT_DATA
 {
 	string first_name;
 	string last_name;
+
+#ifdef PRE_RELEASE
+	string email;
+#endif // PRE_RELEASE
+
 };
 
-#define PRE_RELEASE
 
 int main() {
 #ifdef PRE_RELEASE
-	cout << "the application is running pre-release source code" << endl;
+	cout << "The application is running pre-release source code" << endl;
 	ifstream inputFile("StudentData_Emails.txt");	//reading data from the file
 
 #else 
-	cout << "the application is running standard source code" << endl; 
+	cout << "The application is running standard source code" << endl; 
 	ifstream inputFile("StudentData.txt");	//reading data from the file
 #endif
 
@@ -33,17 +38,32 @@ int main() {
 			//parse data read from file, and store into vector space
 			STUDENT_DATA studentObject;
 			std::istringstream ss(line);
+			
+#ifdef PRE_RELEASE
+			getline(ss, studentObject.last_name, ',');
+			getline(ss, studentObject.first_name, ',');
+			getline(ss, studentObject.email, '\n');
+#else
 			getline(ss, studentObject.last_name, ',');
 			getline(ss, studentObject.first_name, '\n');
+#endif // PRE_RELEASE
+
 			vect.push_back(studentObject);
 		}
+	}
 
 #ifdef _DEBUG		//predefine compiler directive. only run if the program is running in debug mode
 		for (size_t i = 0; i < vect.size(); i++)
 		{
-			cout << vect[i].last_name << ", " << vect[i].first_name << endl;
-		}
-#endif 
+			cout << vect[i].last_name << ", " << vect[i].first_name << " ";
 
-	}
+#ifdef PRE_RELEASE
+			cout << vect[i].email << " ";
+#endif // PRE_RELEASE
+				
+			cout << endl;
+		}
+#endif //_DEBUG
+
+		return 1;
 }
